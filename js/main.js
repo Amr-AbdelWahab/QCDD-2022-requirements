76bf96ebde20221c4fd01c7b544aa9d978839707
Amr Abd el wahab <TO-Head@seg.local>
2022-09-04 15:04:23 +0200

    split html files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+function load(target, url) {
+    var r = new XMLHttpRequest();
+    r.open("GET", url, true);
+    r.onreadystatechange = function () {
+        if (r.readyState != 4 || r.status != 200) return;
+        target.innerHTML = r.responseText;
+    };
+    r.send();
+}
+
+let includes = Array.from(document.querySelectorAll('[data-include]'));
+includes.map(include => {
+    let file = include.dataset['include'] + '.html';
+    load(include, file);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 let goUp = document.getElementById("goUp");
 
